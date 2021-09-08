1ffce7b6f174681b835f10b3b96621e685514937
adel2924 <56393138+adel2924@users.noreply.github.com>
2021-09-08 17:49:04 +0100

    first commit, login done

---
+++ app/src/main/java/com/example/hackathon2021/MainActivity.java
@@ -0,0 +1,49 @@
+package com.example.hackathon2021;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+
+public class MainActivity extends AppCompatActivity {
+
+    private EditText Name;
+    private EditText Password;
+    private Button Login;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        Name = (EditText) findViewById(R.id.etName);
+        Password = (EditText) findViewById(R.id.etPassword);
+        Login = (Button) findViewById(R.id.btnLogin);
+
+        Login.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View view) {
+                if (Name.getText().toString().equals("Admin") && Password.getText().toString().equals("1234")) {
+                    Intent act2Intent = new Intent(MainActivity.this, SecondActivity.class);
+                    startActivity(act2Intent);
+                } else {
+                    Password.setError("Invalid credentials.");
+                }
+            }
+        });
+
+/*    private void checkCredentials(){
+        if(Name.getText().toString().equals("Admin") && Password.getText().toString().equals("1234")){
+            Intent act2Intent = new Intent(MainActivity.this, SecondActivity.class);
+            startActivity(act2Intent);
+        }else{
+            Password.setError("Invalid credentials.");
+        }
+    }*/
+    }
+}
