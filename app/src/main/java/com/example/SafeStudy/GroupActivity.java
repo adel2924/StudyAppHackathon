115a8540bf0e4dbd64af105d6b07b20320cd5613
Lawclaw <clawrage2001@gmail.com>
2021-09-09 21:18:31 +0100

    details in chemistry group (almost done?)

--- app/src/main/java/com/example/SafeStudy/GroupActivity.java
+++ app/src/main/java/com/example/SafeStudy/GroupActivity.java
@@ -10,13 +10,16 @@ import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.TextView;
 
+import java.lang.reflect.Member;
+
 public class GroupActivity extends AppCompatActivity {
 
     private Button Join;
     private Button Leave;
 
-    private ImageView Profilepic;
+    private ImageView ProfilePic;
     private TextView Name;
+    private TextView MemberCount;
 
 
     @Override
@@ -26,16 +29,18 @@ public class GroupActivity extends AppCompatActivity {
 
         Join = (Button) findViewById(R.id.button14);
         Leave = (Button) findViewById(R.id.button15);
-        Profilepic = (ImageView) findViewById(R.id.imageView6);
+        ProfilePic = (ImageView) findViewById(R.id.imageView6);
         Name = (TextView) findViewById(R.id.textView10);
+        MemberCount = (TextView) findViewById(R.id.textView11);
 
         Join.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 Join.setVisibility(View.INVISIBLE);
                 Leave.setVisibility(View.VISIBLE);
-                Profilepic.setVisibility(View.VISIBLE);
+                ProfilePic.setVisibility(View.VISIBLE);
                 Name.setVisibility(View.VISIBLE);
+                MemberCount.setText("3/10");
             }
         });
 
@@ -48,9 +53,10 @@ public class GroupActivity extends AppCompatActivity {
                     @Override
                     public void onClick(DialogInterface dialogInterface, int i) {
                         Leave.setVisibility(View.INVISIBLE);
-                        Profilepic.setVisibility(View.INVISIBLE);
+                        ProfilePic.setVisibility(View.INVISIBLE);
                         Name.setVisibility(View.INVISIBLE);
                         Join.setVisibility(View.VISIBLE);
+                        MemberCount.setText("2/10");
                     }
                 });
                 alertBuilder.setNegativeButton("No", null);
